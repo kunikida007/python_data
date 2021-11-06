24cf6d659038463dc0bff9ca796257015cae4d6a
Yuki Onodera <(maroonv4869@icloud.com)>
2021-11-06 14:25:27 +0900

    2

--- topic-3/2.py
+++ topic-3/2.py
@@ -1,3 +1,8 @@
-x = input()
-for i, j in enumerate(x):
-    print("Case", i, ":", j, sep='')
+list1 = []
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    list1.append(N)
+for i, j in enumerate(list1):
+    print("Case ", i + 1, ":", " ", j, sep="")
